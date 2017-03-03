a53c6d7d7f25eb8c360ee3b6c04510f2e18ade97
Runnan Zhong <r.tina.zhong@gmail.com>
2017-03-03 22:12:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
-  name_hash.each do |k, v|
+  name_hash.each do |name, v|
     if smallest_value == nil || v < smallest_value
       smallest_value = v
-      smallest_key = k
+      smallest_key = name
     end
   end
   smallest_key
